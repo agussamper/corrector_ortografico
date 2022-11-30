1cae846f1c833042a5bd50ee5c6c8478d3ebad19
agus0801 <agus03z@hotmail.com>
2022-11-30 12:17:40 -0300

    documentation

--- spell_checker.h
+++ spell_checker.h
@@ -5,9 +5,10 @@
 
 /**
  * Recibe una TrieNode que representa un diccionario,
- * la ruta de un archivo de texto de entrada y de un archivo
- * salida, si el archivo de salida no existe lo crea.
- * Escribe en el archivo de salida sugerencias para las
+ * la ruta de un archivo de texto de entrada(pathIn) y de 
+ * un archivo salida(pathOut), si el archivo de salida
+ * no existe lo crea.
+ * Escribe en el archivo de salida hasta 5 sugerencias para las
  * palabras del archivo de entrada que no se encuentran
  * en el diccionario.
  */
