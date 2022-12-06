d17bc028a538ad5d566d923afa8e4b884bd06091
agus0801 <agus03z@hotmail.com>
2022-12-06 10:53:52 -0300

    time deleted from main

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "IO.h"
 #include "spell_checker.h"
-#include <time.h> 
 #include "char_arr/char_arr.h"
 #include <ctype.h>
 
@@ -49,15 +48,10 @@ int main(int argc, char const *argv[]) {
   const char* pathIn = argv[1];
   const char* pathOut = argv[2];
   
-  clock_t t = clock();
   TrieNode dic = load_dic("dictionaries/es1.txt");
   spell_check(dic, pathIn, pathOut);  
 
   trie_destroy(dic);
-
-  t = clock() - t;
-  double time_taken = ((double)t)/CLOCKS_PER_SEC; // calculate the elapsed time
-  printf("The program took %f seconds to execute\n", time_taken);
   
   return 0;
 }
